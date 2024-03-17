4252a799c01997a74c3c157f05c861bd86bd616d
BANANAPEEL202 <bbob52645@gmail.com>
2024-03-16 20:21:38 -0700

    added more system support

--- flake.nix
+++ flake.nix
@@ -29,9 +29,21 @@
         overlays = [ self.overlays.default ];
       };
 
+      arch64-darwin_pkgs = import nixpkgs {
+        system = "aarch64-darwin";
+        overlays = [ self.overlays.default ];
+      };
+
+      x86-darwin_pkgs = import nixpkgs {
+        system = "x86_64-darwin";
+        overlays = [ self.overlays.default ];
+      };
+
       packageSets = {
         "x86_64-linux" = makePackageSet x86_pkgs;
         "aarch64-linux" = makePackageSet arm_pkgs;
+        "x86_64-darwin" = makePackageSet x86-darwin_pkgs;
+        "aarch64-darwin" = makePackageSet arch64-darwin_pkgs;
         # Add more systems as needed
       };
     in {
@@ -43,6 +55,39 @@
         # Update the name to something that suites your project.
         name = "nix-devshell";
         packages = with x86_pkgs; [
+          # Development Tools
+          python311Packages.cantools
+          # ht_can_pkg 
+        ];
+
+      };
+
+      devShells.aarch64-linux.default = arm_pkgs.mkShell rec {
+        # Update the name to something that suites your project.
+        name = "nix-devshell";
+        packages = with arm_pkgs; [
+          # Development Tools
+          python311Packages.cantools
+          # ht_can_pkg 
+        ];
+
+      };
+
+      devShells.aarch64-darwin.default = arch64-darwin_pkgs.mkShell rec {
+        # Update the name to something that suites your project.
+        name = "nix-devshell";
+        packages = with arch64-darwin_pkgs; [
+          # Development Tools
+          python311Packages.cantools
+          # ht_can_pkg 
+        ];
+
+      };
+
+      devShells.x86_64-darwin.default = x86-darwin_pkgs.mkShell rec {
+        # Update the name to something that suites your project.
+        name = "nix-devshell";
+        packages = with x86-darwin_pkgs; [
           # Development Tools
           python311Packages.cantools
           # ht_can_pkg 
