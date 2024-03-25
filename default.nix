b78ec0bb665a7e2acbc16f95e92d569f7fedd865
Ben Hall <rcmast3r1@gmail.com>
2024-03-24 20:24:30 -0400

    fixing cantools (#11)

--- default.nix
+++ default.nix
@@ -1,12 +1,21 @@
-{ pkgs, python311Packages }:
+{ pkgs, python311Packages, fetchPypi }:
 
 pkgs.stdenv.mkDerivation rec {
   name = "ht_can_pkg";
-  
+
   src = ./PCAN_project;
-  
-  buildInputs = [ (python311Packages.cantools.overridePythonAttrs (_: { doCheck = false; })) ]; # Python as a build dependency
-  
+
+  buildInputs = [
+    (python311Packages.cantools.overridePythonAttrs (old: {
+      src = fetchPypi {
+        pname = "cantools";
+        version = "39.4.4";
+        hash = "sha256-bo6Ri2ZxpiqfOZBUbs5WI+Hetx3vsc74WplVrDAdqZ4=";
+      };
+      doCheck = false;
+    }))
+  ]; # Python as a build dependency
+
   # Define the build phase to execute the scripts
   buildPhase = ''
     cantools convert hytech.sym hytech.dbc
